6e6c85b4aec6d3c79e76366059aa9b9b5f2f2997
Den41k <daniilkaz1406@gmail.com>
2021-07-15 15:21:42 +0300

    first commit

---
+++ dz (3).py
@@ -0,0 +1,9 @@
+for g in range(13):
+    if g == 3:
+        continue
+    elif g == 5:
+        continue
+    elif g == 8:
+        continue
+    else:
+        print(g)
---
+++ dz.py
@@ -0,0 +1,15 @@
+a = 23
+b = 56
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+name = "Danya"
+age = 15
+game = "genshin impact"
+live = "Vyshneve"
+watch = "anime"
+print(f"My name is {name}, I am {age} years old,my favorite game {game}, I live in {live},I like watch {watch}")
